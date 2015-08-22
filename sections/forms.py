4df927ce852cd1fe46d429c0ad3e6a226aa755a7
Janos Botyanszki <janosb@berkeley.edu>
2015-08-21 17:48:20 -0700

    added earlier times in section form

--- sections/forms.py
+++ sections/forms.py
@@ -2,7 +2,8 @@ from django import forms
 from captcha.fields import CaptchaField
 import inspect
 
-time_choices = ["12:00pm", "12:30pm", "1:00pm", "1:30pm", "2:00pm", "2:30pm", "3:00pm", "3:30pm", "4:00pm",
+time_choices = ["8:00am", "8:30am", "9:00am", "9:30am", "10:00am", "10:30am", "11:00am", "11:30am",
+                "12:00pm", "12:30pm", "1:00pm", "1:30pm", "2:00pm", "2:30pm", "3:00pm", "3:30pm", "4:00pm",
                 "4:30pm", "5:00pm", "5:30pm", "6:00pm", "6:30pm", "7:00pm", "7:30pm", "8:00pm", "8:30pm",
                 "9:00pm", "9:30pm", "10:00pm"]
 
